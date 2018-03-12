d6bd91d0ea667c83fd8ef83e1d1df85b1442d05e
Michael Tiller <michael.tiller@gmail.com>
2018-03-12 15:47:06 -0400

    Added cross-check scripts

---
+++ xc-scripts/src/core/defaults.ts
@@ -0,0 +1,4 @@
+// This is where exported FMUs are stored **in the new per vendor repositories**
+export const exportDir = "Test_FMUs";
+// This is where cross check results are stored **in the new per vendor repositories**
+export const crossCheckDir = "CrossCheck_Results";
---
+++ xc-scripts/src/core/index.ts
@@ -0,0 +1,7 @@
+export * from './defaults';
+export * from './create';
+export * from './exports';
+export * from './imports';
+export * from './process';
+export * from './report';
+export * from './utils';
